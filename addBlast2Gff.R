1c2efe7da83c1716f4d30d19a15cd005db9cd315
Felipe Marques de Almeida <felipemarques89@gmail.com>
2018-08-15 09:59:26 -0300

    update blast2gff

--- addBlast2Gff.R
+++ addBlast2Gff.R
@@ -38,7 +38,7 @@ blastHeader <- c("qseqid", "sseqid", "pident", "length", "mismatch",
 blastFile <- read.table(opt$input, sep = "\t")
 colnames(blastFile) <- blastHeader
 
-att <- paste("Additional database=", opt$database, ";", "DB_ID=", blastFile$sseqid, ";DB_Target=",
+att <- paste("Additional_database=", opt$database, ";", "DB_ID=", blastFile$sseqid, ";DB_Target=",
              blastFile$stitle, sep = "")
 
 ids <- blastFile$qseqid
