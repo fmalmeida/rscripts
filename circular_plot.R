bb55cd8f6dcac861d4889afa06d579aeb110e774
Felipe Marques de Almeida <felipemarques89@gmail.com>
2018-08-21 15:33:29 -0300

    Adding new gviz plots tests

--- circular_plot.R
+++ circular_plot.R
@@ -89,7 +89,7 @@ if( length(unique(gff$seqname)) == 1 ) {
     circle(gr, geom = "ideo", fill = "gray70", radius = 30) +
     circle(gr, geom = "scale", radius = 35, scale.n = scale) +
     circle(gr, geom = "text", aes(label = seqnames), vjust = 0, 
-           size = 3.5, radius = 5, angle = 0) +
+           size = 3.5, radius = 50, angle = 0) +
     theme(legend.position = "bottom")
 } else  {
   p <- ggbio() +
