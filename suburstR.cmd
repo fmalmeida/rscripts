ff9fac4bf256d3e83901b77dfd90fed0b0605e16
Felipe Marques de Almeida <felipemarques89@gmail.com>
2018-08-21 15:05:45 -0300

    Adding new gviz plots tests

--- suburstR.cmd
+++
@@ -1,1 +0,0 @@
-Rscript ~/Documents/bioinfo/rscripts/plot_sunburst.R -i "kpc_merged.gff" -o "teste.html" -p "virulence" -f "feature"
---
+++ testing_Gviz_plots.R
@@ -0,0 +1,47 @@
+#!/usr/bin/Rscript
+suppressMessages(library(Gviz))
+suppressMessages(library(GenomicRanges))
+suppressMessages(library(ballgown))
+suppressMessages(library(DataCombine))
+
+# Teste para plotar features com outros plots além do circular
+
+gff <- gffRead("~/Documents/testes/output_teste/gffs/prokka_gff_with_hmmSources/ncbi_prokka_with_hmmSources.gff")
+gff.gr <- makeGRangesFromDataFrame(gff, keep.extra.columns = FALSE)
+
+##Resistance Features
+gff.gr <- makeGRangesFromDataFrame(gff, keep.extra.columns = FALSE)
+resistance <- grepl.sub(gff, "resistance", "feature")
+res.gr <- makeGRangesFromDataFrame(resistance)
+strand(res.gr) <- "*"
+gff.gr$resistance <-countOverlaps(gff.gr, res.gr)
+subR.gr <- subsetByOverlaps(gff.gr, res.gr)
+strand(subR.gr) <- "*"
+
+##Virulence Features
+gff.gr <- makeGRangesFromDataFrame(gff, keep.extra.columns = FALSE)
+virulence <- grepl.sub(gff, "virulence", "feature")
+vir.gr <- makeGRangesFromDataFrame(virulence)
+strand(vir.gr) <- "*"
+gff.gr$virulence <-countOverlaps(gff.gr, vir.gr)
+subV.gr <- subsetByOverlaps(gff.gr, vir.gr)
+strand(subV.gr) <- "*"
+
+##(ICEs) Features
+gff.gr <- makeGRangesFromDataFrame(gff, keep.extra.columns = FALSE)
+ices <- grepl.sub(gff, "ICE", "feature")
+ices.gr <- makeGRangesFromDataFrame(ices)
+strand(ices.gr) <- "*"
+gff.gr$ices <-countOverlaps(gff.gr, ices.gr)
+subI.gr <- subsetByOverlaps(gff.gr, ices.gr)
+strand(subI.gr) <- "*"
+
+##Plotting
+rtrack <- DataTrack(subR.gr, options(ucscChromosomeNames=FALSE),
+                    name = "Resistance", type=c("heatmap"))
+vtrack <- DataTrack(subV.gr, options(ucscChromosomeNames=FALSE),
+                    name = "Virulence", type=c("heatmap"))
+itrack <- DataTrack(subI.gr, options(ucscChromosomeNames=FALSE),
+                    name = "ICEbergs", type=c("heatmap"))
+gtrack <- GenomeAxisTrack()
+plotTracks(list(gtrack, rtrack, vtrack, itrack), main = "Genomic Features")
