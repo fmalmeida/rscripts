d95235384a7c1201809cd01fdc0462ab817c72e9
fmalmeida <felipemarques89@gmail.com>
2018-10-12 14:20:30 -0300

    changing to docopt

--- installPack.R
+++ installPack.R
@@ -1,9 +1,6 @@
 #!/usr/bin/Rscript
 
 source("https://bioconductor.org/biocLite.R") ;
-#biocLite("Gviz", ask=FALSE, dependencies = TRUE) ;
 biocLite("ballgown", ask=FALSE, dependencies = TRUE) ;
-biocLite("ggbio", ask=FALSE, dependencies = TRUE) ;
 install.packages("sf", repos = "https://cloud.r-project.org/", dependencies = TRUE) ;
-#install.packages(c("DataCombine", "optparse", "plotly", "plyr", "sunburstR"), repos = "https://cloud.r-project.org/", dependencies = TRUE)
-install.packages(c("DataCombine", "glue", "sunburstR"), repos = "https://cloud.r-project.org/", dependencies = TRUE)
+install.packages(c("DataCombine", "glue", "sunburstR", "docopt"), repos = "https://cloud.r-project.org/", dependencies = TRUE)
