f3d609a44e331743a7e91a3ed9441924a7ce2994
Felipe Marques de Almeida <felipemarques89@gmail.com>
2018-11-05 21:58:14 -0200

    Add rsamtools

--- installPack.R
+++ installPack.R
@@ -1,8 +1,14 @@
 #!/usr/bin/Rscript
 
-source("https://bioconductor.org/biocLite.R") ;
-biocLite("BiocUpgrade") ; 
-biocLite("Rsamtools", ask=FALSE, dependencies = TRUE);
+#source("https://bioconductor.org/biocLite.R") ;
+#biocLite("BiocUpgrade") ; 
+#biocLite("Rsamtools", ask=FALSE, dependencies = TRUE);
 install.packages("sf", repos = "https://cloud.r-project.org/", dependencies = TRUE) ;
 install.packages(c("DataCombine", "glue", "sunburstR", "docopt", "dplyr"), repos = "https://cloud.r-project.org/", dependencies = TRUE) ;
-biocLite("ballgown", ask=FALSE, dependencies = TRUE) ;
+#biocLite("ballgown", ask=FALSE, dependencies = TRUE) ;
+
+# Install bioc packages
+
+install.packages("BiocManager", repos = "https://cloud.r-project.org/", dependencies = TRUE) ;
+BiocManager::install("Rsamtools", ask = FALSE) ;
+BiocManager::install("ballgown", ask = FALSE) ;
