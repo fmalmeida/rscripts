27b33dfa9a9b7e3163b189963970a4dad767f602
Felipe Marques de Almeida <felipemarques89@gmail.com>
2018-11-05 15:45:21 -0200

    Add rsamtools

--- installPack.R
+++ installPack.R
@@ -1,6 +1,7 @@
 #!/usr/bin/Rscript
 
 source("https://bioconductor.org/biocLite.R") ;
+biocLite("Rsamtools", ask=FALSE, dependencies = TRUE);
 biocLite("ballgown", ask=FALSE, dependencies = TRUE) ;
 install.packages("sf", repos = "https://cloud.r-project.org/", dependencies = TRUE) ;
 install.packages(c("DataCombine", "glue", "sunburstR", "docopt", "dplyr"), repos = "https://cloud.r-project.org/", dependencies = TRUE)
