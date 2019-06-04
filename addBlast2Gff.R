fad8bfde6ad43239d9649077b6cde3264d1c948f
Felipe Marques de Almeida <felipemarques89@gmail.com>
2019-06-04 12:02:17 -0300

    Adding Bitscore filter

--- addBlast2Gff.R
+++ addBlast2Gff.R
@@ -63,6 +63,7 @@ if (nrow(blastFile) > 0) {
   
 # Remove duplicates based on bitscore
 blastFile <- blastFile[order(blastFile$qseqid, -abs(blastFile$bitscore) ), ]
+blastFile <- dplyr::filter(blastFile, bitscore >= 500)
 blastFile <-blastFile[ !duplicated(blastFile$qseqid), ]
 blastFile <- blastFile[order(blastFile$qseqid),]
 
