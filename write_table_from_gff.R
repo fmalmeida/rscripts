e8d1e0ada29114530aea0db199cddbe3414e6a62
fmalmeida <felipemarques89@gmail.com>
2019-08-10 15:38:13 -0300

    First CARD try

--- write_table_from_gff.R
+++ write_table_from_gff.R
@@ -19,21 +19,6 @@ suppressMessages(library(DataCombine))
 suppressMessages(library(ballgown))
 suppressMessages(library(stringr))
   
-# Function used to get values from attributes column. It was completely edited
-# to keep all additional findings. I may have to implement on other scripts.
-# getAttributeField <- function (df, field, attrsep = ";") {
-#  s = strsplit(df, split = attrsep, fixed = TRUE)
-#  v <- sapply(s, function(x) {
-#    v = str_subset(x, pattern=field)
-#    y = strsplit(v, split = "=", fixed = TRUE)
-#    m = sapply(y, "[", 2)
-#    if (length(m) > 0) {
-#    rv = paste(unique(m), collapse = ",") } else { 
-#      rv = as.character(NA) 
-#    } 
-#  })
-#  return(v)
-#}
 getAttributeField <- function (x, field, attrsep = ";") { 
   s = strsplit(x, split = attrsep, fixed = TRUE) 
   sapply(s, function(atts) { 
@@ -98,6 +83,7 @@ if (length(opt$type) && opt$type != "card") {
   gff$Name <- getAttributeField(gff$attributes, "db_name", ";")
   gff$Drug_Class <- getAttributeField(gff$attributes, "drug_class", ";")
   gff$Resistance_Mechanism <- getAttributeField(gff$attributes, "resistance_mechanism", ";")
+  gff$CVTerm <- getAttributeField(gff$attributes, "cvterm", ";")
   gff$Domain <- getAttributeField(gff$attributes, "protein_motif", ";")
   gff$Prokka_product <- getAttributeField(gff$attributes, "product", ";")
   gff$Prokka_ID <- getAttributeField(gff$attributes, "id", ";")
@@ -107,7 +93,7 @@ if (length(opt$type) && opt$type != "card") {
 
   #### Give columns a name
   col = c("seqname", "start", "end", "feature", "source", "ARO_Accession", 
-        "Gene_Family", "Name", "Drug_Class", "Resistance_Mechanism", 
+        "Gene_Family", "Name", "Drug_Class", "Resistance_Mechanism", "CVTerm",
         "Domain", "Prokka_product", "Prokka_ID", "Prokka_inference", "Additional_DB", "Additional_product")
   
   ### Write document
