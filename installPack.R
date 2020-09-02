336f5b6bf84bd6ec995d32a3e1fea664c03cbeb0
fmalmeida <felipemarques89@gmail.com>
2020-09-02 10:55:38 -0300

    Updating rscripts

--- installPack.R
+++ installPack.R
@@ -2,12 +2,11 @@
 
 install.packages("sf", repos = "https://cloud.r-project.org/", dependencies = TRUE) ;
 install.packages(c("DataCombine", "glue", "docopt", "dplyr", 
-                   "stringr", "kableExtra", "ggplot2",
+                   "stringr", "DT", "ggplot2",
                    "bookdown", "plyr", "tidyr"), repos = "https://cloud.r-project.org/", dependencies = TRUE) ;
 
 
 # Install bioc packages
-
 install.packages("BiocManager", repos = "https://cloud.r-project.org/", dependencies = TRUE) ;
 BiocManager::install("Rsamtools", ask = FALSE) ;
 BiocManager::install("ballgown", ask = FALSE)
