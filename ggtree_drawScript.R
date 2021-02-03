78eaa30a0b3a43c738e0f92d75f7791212ce2fd6
fmalmeida <felipemarques89@gmail.com>
2021-02-03 19:12:44 -0300

    Update ggtree template

--- ggtree_drawScript.R
+++ ggtree_drawScript.R
@@ -34,6 +34,8 @@ in_tree$tip.label <- shorter_names(in_tree$tip.label)
 # Kv15 was used as ref ... we need to change their name
 in_tree$tip.label <- gsub(pattern = ".ref", replacement = "", fixed = T,
      x = in_tree$tip.label)
+in_tree$tip.label <- gsub(pattern = ".fna", replacement = "", fixed = T,
+                          x = in_tree$tip.label)
 
 ##########################################
 ### Get metadata -- fetching countries ###
@@ -85,7 +87,6 @@ p <- tree1 %>%
   geom_tiplab(align = T)
 p
 
-
 #########################################################
 ### As we can see, a lot of leaves contain >5 genomes ###
 ### side-by-side indicating few snps between them.    ###
@@ -131,11 +132,10 @@ p <- reduced_tree %>%
     size = 2.5,
     linesize = .5,
     align = TRUE,
-    aes(subset=label %in% c("Kv15.fna", "Kv35.fna", "Kv57.fna", "Kv97.fna", "Kv104.fna",
+    aes(subset=label %in% c("Kv15", "Kv35", "Kv57", "Kv97", "Kv104",
                             "GCF_001261855.1", "GCF_004312565.1", "GCF_003255775.1", "GCF_003195165.1"))
   )
 p
-
 
 #################################################
 ### The colors are not so good. Let's make it ###
@@ -145,6 +145,7 @@ p
 custom_palette <- 
   createPalette(N = length(unique(reduced_tree$tip.country)),
                 seedcolors = c('#7fc97f','#beaed4','#fdc086','#ffff99','#386cb0','#f0027f','#bf5b17','#666666'))
+
 # Plot with custom palette
 p <- reduced_tree %>%
   ggtree(layout='circular') %<+% dd +
@@ -153,7 +154,7 @@ p <- reduced_tree %>%
     size = 2.5,
     linesize = .5,
     align = TRUE,
-    aes(subset=label %in% c("Kv15.fna", "Kv35.fna", "Kv57.fna", "Kv97.fna", "Kv104.fna",
+    aes(subset=label %in% c("Kv15", "Kv35", "Kv57", "Kv97", "Kv104",
                             "GCF_001261855.1", "GCF_004312565.1", "GCF_003255775.1", "GCF_003195165.1"))) + 
   scale_color_manual(values = unname(custom_palette))
 p
@@ -163,7 +164,6 @@ p
 #############################################
 ggsave(filename = "./ggtree_test.png", plot = p, device = "png")
 ggsave(filename = "./ggtree_test.svg", plot = p, device = "svg")
-
 
 ################################################################
 ### Now let's compare the genome content information between ###
@@ -177,12 +177,12 @@ gene_matrix <- read.table("./gene_presence_absence.Rtab", header = TRUE, strings
 
 # Let's sort this matrix
 attach(gene_matrix)
-gene_matrix <- gene_matrix[order(Gene),]
+gene_matrix <- gene_matrix[order(Gene_Class, Gene),]
 detach(gene_matrix)
 
 # We need to transpose this matrix
 # in order to plot it
-gene_matrix_t <- t(gene_matrix)
+gene_matrix_t <- gene_matrix %>% select(-Gene_Class) %>% t()
 
 # Get colnames (genome names)
 colnames(gene_matrix_t) <- gene_matrix_t[1,]
@@ -192,98 +192,61 @@ gene_matrix_t <- gene_matrix_t[-1,]
 rownames(gene_matrix_t) <- shorter_names(rownames(gene_matrix_t)) %>%
   lapply(function(t){
     gsub(x = t, pattern = "gbk", replacement = "fna", fixed = T)
+    gsub(x = t, pattern = ".gbk", replacement = "", fixed = T)
   })
 
-# Change label from number to category
-gene_matrix_t[gene_matrix_t==0] <- "absent"
-gene_matrix_t[gene_matrix_t==1] <- "present"
-
-# Reload tree
-p <- reduced_tree %>%
-  ggtree(layout='rectangular', branch.length = "none")
-
-# Plot heatmap
-gheatmap(p, gene_matrix_t, colnames_angle = 90, font.size = 3, offset = 1, colnames_offset_y = 15, 
-         colnames_position = "top") +
-  scale_fill_manual(values=c("#f63737", "#c1eaea"))
-
-
-#######################################################
-### Albeit it shows a nice overview, we can't infer ###
-### the distance of the genomes based on the gene   ###
-### content. For that, we need to re-calculate the  ###
-### tree based on the gene presence matrix as source###
-### of distances                                    ###
-#######################################################
-# We need to transpose this matrix
-# in order to plot it
-gene_matrix_t <- t(gene_matrix)
-
-# Get colnames (genome names)
-colnames(gene_matrix_t) <- gene_matrix_t[1,]
-gene_matrix_t <- gene_matrix_t[-1,]
-
-# Small fix on names
-rownames(gene_matrix_t) <- shorter_names(rownames(gene_matrix_t)) %>%
-  lapply(function(t){
-    gsub(x = t, pattern = "gbk", replacement = "fna", fixed = T)
-  })
+# Remove genomes not used before
+gene_matrix_t <- gene_matrix_t[row.names(gene_matrix_t) %in% as.list(reduced_tree$tip.label), ]
 
 # Calc dist
-gene_dist <- dist(gene_matrix_t, method = "manhattan")
+gene_dist <- dist(gene_matrix_t, method = "euclidean")
 
 # Generate tree
-cluster <- hclust(gene_dist)
+cluster <- hclust(gene_dist, method = "single")
 tree2 <- as.phylo(cluster)
-
-# Check hosts
-first_filter <- metadata[metadata$host != "Homo sapiens", ]
-second_filter <- first_filter[!grepl("human|urine", first_filter$isolation_source), ]
-third_filter <- second_filter[(!grepl("missing", second_filter$isolation_source) | 
-                               !grepl("missing", second_filter$host)), ]
-final <- third_filter[!grepl("not collected|unknown|respiratory|animal|Chilli|Masala|milk|Chicken", third_filter$isolation_source), ]
-from_env <- final$genome
 
 # Reload tree
 grp <- list(
-  our_samples = c("Kv15.fna", "Kv35.fna", "Kv57.fna", "Kv97.fna", "Kv104.fna"),
-  environmental = from_env
+  our_samples = c("Kv15", "Kv35", "Kv57", "Kv97", "Kv104")
 )
-p <- tree2 %>%
-  groupOTU(grp) %>%
-  ggtree(layout='rectangular', aes(color=group),
-         branch.length = 'none') +
-  scale_color_manual(values = c("black", "darkgreen", "firebrick"),
+
+# Drop tips from the bigger leaf (with lots of repetition)
+reduced_tree2 <- drop.tip(tree2, readLines("./ggtree_genomes_to_drop_for_heatmap.txt")) %>%
+  groupOTU(grp)
+
+# Check results
+p <- reduced_tree2 %>%
+  ggtree(layout='rectangular', aes(color=group), branch.length = 'none') +
+  scale_color_manual(values = c("black", "firebrick"),
                      name = "Groups",
-                     guide = 'none')
+                     guide = 'none') +
+  geom_tiplab(
+    size = 2.5,
+    linesize = .5,
+    align = TRUE,
+    aes(subset=label %in% c("Kv15", "Kv35", "Kv57", "Kv97", "Kv104")))
 p
-
-# Remove some repetitive genomes
-sampled <- list.sample(get_taxa_name(p, 478), 50) # Node 478
-sampled <- rbind(sampled, list.sample(get_taxa_name(p, 323), 15)) # Node 323
-#sampled <- rbind(sampled, list.sample(get_taxa_name(p, 383), 15)) # Node 383
-#sampled <- rbind(sampled, list.sample(get_taxa_name(p, 413), 15)) # Node 383
-
-# Drop sampled tips
-reduced_tree2 <- tree2 %>%
-  drop.tip(sampled) %>%
-  groupOTU(grp) %>%
-  ggtree(layout='rectangular', aes(color=group),
-         branch.length = 'none') +
-  scale_color_manual(values = c("black", "darkgreen", "firebrick"),
-                     name = "Groups",
-                     guide = 'none')
-
-reduced_tree2 + geom_nodepoint()
 
 # Change label from number to category
 gene_matrix_t[gene_matrix_t==0] <- "absent"
 gene_matrix_t[gene_matrix_t==1] <- "present"
 
+# Change colors by gene class
+classes <- gene_matrix %>% select(Gene_Class) %>% unique()
+for (i in as.list(classes$Gene_Class)) {
+  genes <- gene_matrix %>% filter(Gene_Class == i) %>% select(Gene) %>% unique()
+  
+  for (u in as.list(genes$Gene)) {
+    gene_matrix_t[gene_matrix_t[, u] == "present", u] <- 
+      gsub("present", i, gene_matrix_t[gene_matrix_t[, u] == "present", u])
+  }
+
+}
+
 # Plot with heatmap
-heat_p <- gheatmap(reduced_tree2, gene_matrix_t, colnames_angle = 90, font.size = 2.5, offset = 1.5, colnames_offset_y = 15, 
-         colnames_position = "top") +
-  scale_fill_manual(values = c("#f63737", "#c1eaea"),
+heat_p <- gheatmap(p, gene_matrix_t, colnames_angle = 45, font.size = 2.5, offset = 10, colnames_offset_y = 0.5, 
+         colnames_position = "top", color = "black", hjust = 0, width = 2) +
+  scale_fill_manual(values=c('white', 'lightgray', '#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69'),
                     name   = "Virulence genes") +
   theme(
     legend.background=element_rect(fill=NA),
@@ -293,5 +256,117 @@ heat_p <- gheatmap(reduced_tree2, gene_matrix_t, colnames_angle = 90, font.size 
 heat_p
 
 # Save plots
-ggsave(filename = "./ggtree_test_virulence_genes.png", plot = heat_p, device = "png")
-ggsave(filename = "./ggtree_test_virulence_genes.svg", plot = heat_p, device = "svg")
+ggsave(filename = "./ggtree_virulence_genes_colored.png", plot = heat_p, device = "png")
+ggsave(filename = "./ggtree_virulence_genes_colored.svg", plot = heat_p, device = "svg")
+
+###########################################
+### Plot virulence genes with countries ###
+###########################################
+# Load gene presence information as matrix
+gene_matrix <- read.table("./gene_presence_absence.Rtab", header = TRUE, stringsAsFactors = F)
+
+# Let's sort this matrix
+attach(gene_matrix)
+gene_matrix <- gene_matrix[order(Gene_Class, Gene),]
+detach(gene_matrix)
+
+# We need to transpose this matrix
+# in order to plot it
+gene_matrix_t <- gene_matrix %>% select(-Gene_Class) %>% t()
+
+# Get colnames (genome names)
+colnames(gene_matrix_t) <- gene_matrix_t[1,]
+gene_matrix_t <- gene_matrix_t[-1,]
+
+# Small fix on names
+rownames(gene_matrix_t) <- shorter_names(rownames(gene_matrix_t)) %>%
+  lapply(function(t){
+    gsub(x = t, pattern = "gbk", replacement = "fna", fixed = T)
+    gsub(x = t, pattern = ".gbk", replacement = "", fixed = T)
+  })
+
+# Remove genomes not used before
+gene_matrix_t <- gene_matrix_t[row.names(gene_matrix_t) %in% as.list(reduced_tree$tip.label), ]
+
+# Calc dist
+gene_dist <- dist(gene_matrix_t, method = "euclidean")
+
+# Generate tree
+cluster <- hclust(gene_dist, method = "single")
+tree3 <- as.phylo(cluster)
+
+# Reload tree
+grp <- list(
+  our_samples = c("Kv15", "Kv35", "Kv57", "Kv97", "Kv104")
+)
+
+# Drop tips from the bigger leaf (with lots of repetition)
+reduced_tree3 <- drop.tip(tree3, readLines("./ggtree_genomes_to_drop_for_heatmap.txt")) %>%
+  groupOTU(grp)
+
+# Fetch countries
+reduced_tree3$tip.country <- get_countries(reduced_tree3, metadata)
+
+# Transform in data.frame
+dd2 <- do.call(rbind, Map(data.frame, node=1:length(reduced_tree3$tip.label), Country=reduced_tree3$tip.country))
+
+# Sort countries
+dd2$Country <- as.character(dd2$Country)
+dd2 <- dd2[order(dd2$Country),]
+dd2$Country <- as.factor(dd2$Country)
+
+# Remove row.names
+row.names(dd2) <- NULL
+
+# Check results
+custom_palette <- 
+  createPalette(N = 33,
+                seedcolors = c('#b22222', '#7fc97f','#beaed4',
+                               '#fdc086','#ffff99','#386cb0',
+                               '#f0027f','#bf5b17','#666666'))
+p <- reduced_tree3 %>%
+  ggtree(layout='rectangular', branch.length = 'none') %<+% dd2 +
+  geom_tippoint(aes(color=Country), size=1.2, alpha=1.5) +
+  scale_color_manual(values = c(unname(custom_palette), 'black')) +
+  geom_tiplab(
+    size = 4,
+    linesize = .5,
+    offset = 0.2,
+    align = TRUE,
+    aes(subset=label %in% c("Kv15", "Kv35", "Kv57", "Kv97", "Kv104")))
+p
+
+# Change label from number to category
+gene_matrix_t[gene_matrix_t==0] <- "absent"
+gene_matrix_t[gene_matrix_t==1] <- "present"
+
+# Change colors by gene class
+classes <- gene_matrix %>% select(Gene_Class) %>% unique()
+for (i in as.list(classes$Gene_Class)) {
+  genes <- gene_matrix %>% filter(Gene_Class == i) %>% select(Gene) %>% unique()
+  
+  for (u in as.list(genes$Gene)) {
+    gene_matrix_t[gene_matrix_t[, u] == "present", u] <- 
+      gsub("present", i, gene_matrix_t[gene_matrix_t[, u] == "present", u])
+  }
+  
+}
+
+# Plot with heatmap
+heat_p <- gheatmap(p, gene_matrix_t, colnames_angle = 45, font.size = 4.5, offset = 10, colnames_offset_y = 0.5, 
+                   colnames_position = "top", color = "black", hjust = 0, width = 3.5) +
+  scale_fill_manual(values=c('white', 'lightgray', '#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69'),
+                    name   = "Virulence genes") +
+  theme(
+    legend.background=element_rect(fill=NA),
+    legend.title=element_text(size=13), # The size should be adjusted with different devout.
+    legend.text=element_text(size=11),
+    legend.spacing.y = unit(0.25, "cm")) +
+  ylim(0,185) +
+  guides(colour = guide_legend(override.aes = list(size=3))) # adjust size of dots in legend
+heat_p
+
+# Save plots
+ggsave(filename = "./ggtree_virulence_genes_colored_with_countries.png", plot = heat_p, device = "png")
+ggsave(filename = "./ggtree_virulence_genes_colored_with_countries.svg", plot = heat_p, device = "svg", 
+       height = 15, width = 20)
